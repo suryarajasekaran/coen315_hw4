6e88157d61333f8c68925cd25ac3d2c9674298d2
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-02-27 22:10:13 -0800

    basic backend complete : commit

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+from flask_restful import Api
+import json
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/ping')
+def ping():
+    return json.dumps({"status":True})
+
+@app.route('/station')
+def station():
+    arg_source = request.args.get('source')
+    if arg_source is None:
+        return json.dumps({"status": False})
+    else:
+        url="http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig="+arg_source+"&key=MW9S-E7SL-26DU-VV8V&json=y"
+        response = requests.get(url)
+        return json.dumps({"data": response.json()["root"]["stations"]["station"]})
+
+@app.route('/stations')
+def stations():
+    url="http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y"
+    response = requests.get(url)
+    return json.dumps({"data":response.json()["root"]["stations"]["station"]})
+
+@app.route('/trips')
+def trips():
+    arg_source = request.args.get('source')
+    arg_dest = request.args.get('dest')
+    if arg_source is None or arg_source is None:
+        return json.dumps({"status": False})
+    else:
+        #url_fare="http://api.bart.gov/api/sched.aspx?cmd=fare&orig=12th&dest=embr&date=today&key=MW9S-E7SL-26DU-VV8V&json=y"
+        #url_etd = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + arg_source + "&key=MW9S-E7SL-26DU-VV8V&json=y"
+        url_eta = "http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=" + arg_source + "&dest=" + arg_dest + "&key=MW9S-E7SL-26DU-VV8V&json=y"
+        response = requests.get(url_eta)
+        return json.dumps({"data":response.json()["root"]["schedule"]["request"]["trip"]})
+
+@app.route('/etd')
+def etd():
+    arg_source = request.args.get('source')
+    if arg_source is None:
+        return json.dumps({"status": False})
+    else:
+        url_etd = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig="+arg_source+"&key=MW9S-E7SL-26DU-VV8V&json=y"
+        response = requests.get(url_etd)
+        return json.dumps({"data":response.json()["root"]["station"]})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8881, debug=True)
