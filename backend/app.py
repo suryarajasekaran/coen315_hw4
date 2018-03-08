d29f098fe200c7a776cf0e9d7c1b000bd1ae6e20
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-08 04:00:00 -0800

    Dockerize

--- backend/app.py
+++ backend/app.py
@@ -53,7 +53,7 @@ def etd():
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'http://suryarajasekaran:8882')
+  response.headers.add('Access-Control-Allow-Origin', 'http://suryarajasekaran.com:8882')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
