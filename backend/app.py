f2f402506d05d880411af152034ab0f24086e5be
skasabar <skasabar@cisco.com>
2018-03-08 22:06:55 -0800

    cors fix

--- backend/app.py
+++ backend/app.py
@@ -63,7 +63,7 @@ def etd():
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin', origin_str))
+  #response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin', origin_str))
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
